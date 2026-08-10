796c75ed5be4ccde0e5169c90d5022ef1baadcbe
Samuel Abraham <128758030+Shine0078@users.noreply.github.com>
2026-08-10 07:20:35 -0400

    build(android): enable notification plugin desugaring

--- apps/mobile/android/app/build.gradle.kts
+++ apps/mobile/android/app/build.gradle.kts
@@ -20,6 +20,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -70,5 +71,6 @@ flutter {
 }
 
 dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
     implementation("com.plaid.link:sdk-core:6.2.0")
 }
